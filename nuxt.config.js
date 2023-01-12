3bb3c1490379263d4a729d7fa60a0381879a796f
Florian Wartner <florian.wartner@cloud37.io>
2023-01-12 11:12:23 +0100

    Changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    // baseURL: '/',
+    baseURL: 'https://main--fanciful-beignet-5d7b89.netlify.app/',
     proxy: true
   },
 
